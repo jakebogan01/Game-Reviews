149e19fcadb8d5aed1c4b3c400d1336ee113b84b
Jake Bogan <theronbogan1@gmail.com>
2021-05-21 16:13:51 -0400

    First Commit

--- resources/views/home.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    <h1 class="text-blue-500">Testing</h1>
-@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\Auth\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
+Route::get('/dashboard',[DashboardController::class,'index'])->name('dashboard');
+
+Route::get('/register',[RegisterController::class,'index'])->name('register');
+Route::post('/register',[RegisterController::class,'store']);
+
+Route::get('/register',[RegisterController::class,'index'])->name('register');
+Route::post('/register',[RegisterController::class,'store']);
+
+Route::get('/posts', function (){
+    return view('posts.index');
 });
